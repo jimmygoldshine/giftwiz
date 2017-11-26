9d89254a33ab9f8fab143da6fcd3c0fc28644d6a
James <james.dix@hullabalook.com>
2017-11-26 16:15:31 +0000

    ingest data extraction and object creation - tests passing

--- app/management/commands/ingest.py
+++ app/management/commands/ingest.py
@@ -8,3 +8,31 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         print 'ingesting'
+
+    def extract_row(self, csv_row):
+        return csv_row.split('|')
+
+
+    def data_to_object(self, product_data):
+        indexes = {
+            'id': 0,
+            'product_name': 1,
+            'sku': 2,
+            'primary_categroy': 3,
+            'secondary_category': 4,
+            'product_url': 5,
+            'product_image_url': 6,
+            'buy_url': 7,
+            'short_product_description': 8,
+            'long_product_description': 9,
+            'discount': 10,
+            'discount_type': 11,
+            'sale_price': 12,
+            'retail_price': 13,
+            'currency': 25
+        }
+
+        return {'name': product_data[indexes['product_name']],
+            'image': product_data[indexes['product_image_url']],
+            'price': product_data[indexes['retail_price']],
+            'url': product_data[indexes['product_url']]}
